8a05b140e3ac08c4912051a6924a639a25a2dfc7
isaias.neto <isaias.neto@pcinformatica.com.br>
2018-01-10 15:18:34 -0200

    Aula 03 - Classificação de variáveis categóricas

--- dados.py
+++ dados.py
@@ -7,7 +7,7 @@ def carregar_acessos():
     #Y as marcacoes (que eu nao sei) eh o que eu quero prever
     Y = []
 
-    arquivo = open('acesso_pagina.csv', 'rb')
+    arquivo = open('acessos.csv', 'rb')
     leitor = csv.reader(arquivo)
 
     leitor.next()
@@ -15,6 +15,24 @@ def carregar_acessos():
     for home, como_funciona, contato, comprou in leitor:
 
         dado = [int(home), int(como_funciona), int(contato)]
+        X.append(dado)
+        Y.append(int(comprou))
+
+    return X, Y
+
+def carregar_buscas():
+
+    X = []
+    Y = []
+
+    arquivo = open('buscas.csv', 'rb')
+    leitor = csv.reader(arquivo)
+
+    leitor.next()
+
+    for home, busca, logado, comprou in leitor:
+
+        dado = [int(home), busca, int(logado)]
         X.append(dado)
         Y.append(int(comprou))
 
