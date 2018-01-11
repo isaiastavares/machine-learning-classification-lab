7d94b08d556eb6fbec7b1589794213f2677b14d2
isaias.neto <isaias.neto@pcinformatica.com.br>
2018-01-11 19:03:25 -0200

    Aula 04 - Classificando os textos e ganhando produtividade na empresa

---
+++ classifica_emails.py
@@ -0,0 +1,106 @@
+#!-*- coding: utf8 -*-
+from collections import Counter
+from sklearn.cross_validation import cross_val_score
+
+import numpy as np
+import pandas as pd
+
+classificacoes = pd.read_csv('emails.csv')
+textosPuros = classificacoes['email']
+textosQuebrados = textosPuros.str.lower().str.split(' ')
+
+dicionario = set()
+for lista in textosQuebrados:
+    dicionario.update(lista)
+
+totalDePalavras = len(dicionario)
+tuplas = zip(dicionario, xrange(totalDePalavras))
+
+tradutor = {palavra:indice for palavra, indice in tuplas}
+
+def vetorizar_texto(texto, tradutor):
+    vetor = [0] * len(tradutor)
+    for palavra in texto:
+        if palavra in tradutor:
+            posicao = tradutor[palavra]
+            vetor[posicao] += 1
+    return vetor
+
+vetoresDeTexto = [vetorizar_texto(texto, tradutor) for texto in textosQuebrados]
+marcas = classificacoes['classificacao']
+
+X = np.array(vetoresDeTexto)
+Y = np.array(marcas.tolist())
+
+porcentagem_de_treino = 0.8
+
+tamanho_do_treino = int(porcentagem_de_treino * len(Y))
+tamanho_da_validacao = len(Y) - tamanho_do_treino
+
+treino_dados = X[0:tamanho_do_treino]
+treino_marcacoes = Y[0:tamanho_do_treino]
+
+validacao_dados = X[tamanho_do_treino:]
+validacao_marcacoes = Y[tamanho_do_treino:]
+
+def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes):
+    k = 10
+    scores = cross_val_score(modelo, treino_dados, treino_marcacoes, cv = k)
+    taxa_de_acerto = np.mean(scores)
+
+    msg = "Taxa de acerto do {0}: {1}".format(nome, taxa_de_acerto)
+    print msg
+    return taxa_de_acerto
+
+def teste_real(modelo, validacao_dados, validacao_marcacoes):
+    resultado = modelo.predict(validacao_dados)
+
+    acertos = resultado == validacao_marcacoes
+
+    total_de_acertos = sum(acertos)
+    total_de_elementos = len(validacao_marcacoes)
+
+    taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+    msg = "Taxa de acerto do vencedor entre os dois algoritmos no mundo real: {0}".format(taxa_de_acerto)
+    print(msg)
+
+resultados = {}
+
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+modeloOneVsRest = OneVsRestClassifier(LinearSVC(random_state = 0))
+resultadoOneVsRest = fit_and_predict("OneVsRest", modeloOneVsRest, treino_dados, treino_marcacoes)
+resultados[resultadoOneVsRest] = modeloOneVsRest
+
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.svm import LinearSVC
+modeloOneVsOne = OneVsOneClassifier(LinearSVC(random_state = 0))
+resultadoOneVsOne = fit_and_predict("OneVsOne", modeloOneVsOne, treino_dados, treino_marcacoes)
+resultados[resultadoOneVsOne] = modeloOneVsOne
+
+from sklearn.naive_bayes import MultinomialNB
+modeloMultinomial = MultinomialNB()
+resultadoMultinomial = fit_and_predict("MultinomialNB", modeloMultinomial, treino_dados, treino_marcacoes)
+resultados[resultadoMultinomial] = modeloMultinomial
+
+from sklearn.ensemble import AdaBoostClassifier
+modeloAdaBoost = AdaBoostClassifier()
+resultadoAdaBoost = fit_and_predict("AdaBoostClassifier", modeloAdaBoost, treino_dados, treino_marcacoes)
+resultados[resultadoAdaBoost] = modeloAdaBoost
+
+maximo = max(resultados)
+vencedor = resultados[maximo]
+
+print "Vencedor: "
+print vencedor
+
+vencedor.fit(treino_dados, treino_marcacoes)
+
+teste_real(vencedor, validacao_dados, validacao_marcacoes)
+
+acerto_base = max(Counter(validacao_marcacoes).itervalues())
+taxa_de_acerto_base = 100.0 * acerto_base / len(validacao_marcacoes)
+
+print("Taxa de acerto base: %.2f" % taxa_de_acerto_base)
+print("Total de testes: %d" % len(validacao_dados))
